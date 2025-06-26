1aeeece59d186adc869d68930f8b852ff294ff9a
Douglas G. <twowheeleddev@gmail.com>
2025-06-25 22:02:45 -0400

    Update event-handler-factory.js

--- event-handler-factory.js
+++ event-handler-factory.js
@@ -4,6 +4,8 @@
  * @param {string} initialLabel A unique label for this handler
  * @returns {{
  * handler: function(Event): void,
+ * subscribe: (element: Element, eventType?: string) => void,
+ * unsubscribe: () => void,
  * getCount: function(): number,
  * reset: function(): void
  * setLabel: function(string): void
@@ -12,6 +14,8 @@
 function createClickTracker(initialLabel) {
   let count = 0;
   let label = initialLabel;
+  let targetElement = null;
+  let eventType = null;
 
   function handler(event) {
     count += 1;
@@ -24,6 +28,36 @@ function createClickTracker(initialLabel) {
 
   return {
     handler,
+
+    /**
+     *
+     * Attach the handler to a DOM element.
+     * if already subscribed, it will first unsubscribe.
+     *
+     * @param {Element} element The DOM element to attach the handler to
+     * @param {string} [eventType='click'] The type of event to listen
+     */
+    subscribe(element, type = "click") {
+      if (targetElement && eventType) {
+        this.unsubscribe();
+      }
+      targetElement = element;
+      eventType = type;
+      targetElement.addEventListener(eventType, handler);
+    },
+
+    /**
+     *
+     * Remove the handler from whatever it was subscribed to.
+     *
+     */
+    unsubscribe() {
+      if (targetElement && eventType) {
+        targetElement.removeEventListener(eventType, handler);
+        targetELement = null;
+        eventType = null;
+      }
+    },
     getCount() {
       return count;
     },
@@ -45,8 +79,15 @@ document
   .getElementById("saveButton")
   .addEventListener("click", tracker.handler);
 
+// Subscribe
+saveTracker.subscribe(btnSave);
+
 // examples continued
+console.log(saveTracker.getCount());
 console.log(tracker.getCount()); // 0
 tracker.reset();
 tracker.setLabel("quick-save");
 console.log(tracker.getLabel());
+
+// Unsubscribe
+tracker.unsubscribe();
